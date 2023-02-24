1fbb7009f6e51b9f50d69b1fbecf54cf0133a1d4
HanadiAbouHamdan <hanadi.abouhamdan97@hotmail.com>
2023-02-24 23:21:14 +0200

    Adding JS for changing the dices and stating the one who win

--- dice.js
+++ dice.js
@@ -1,19 +1,39 @@
-var button=document.getElementsByClassName('btn');
-var txt=doxument.getElementsByClassName('header');
 
 
+var button1=document.getElementById('btn1');
+var heading=document.getElementById("myHeading");
 
 
+var dicepic=["img/Dice1.png","img/Dice2.png","img/Dice3.png","img/Dice4.png","img/Dice5.png","img/Dice6.png"];//initializing array
 
 
+function play(){
 
 
-if (dice1>dice2){
-    txt.textContent="Player 1 wins!"
+var randompic1= Math.floor(Math.random() * dicepic.length);//index
+console.log(randompic1);
+var randompic2=Math.floor(Math.random() * dicepic.length);//index
+console.log(randompic1,randompic2);
+
+var t1=document.getElementsByTagName("img")[0];
+t1.setAttribute("src",dicepic[randompic1]);
+
+var t2=document.getElementsByTagName("img")[1];
+t2.setAttribute("src",dicepic[randompic2]);
+
+
+if(randompic1>randompic2){
+//heading.innerHTML="Player 1 wins!";
+console.log("player 1 wins")
 }
-else if (dice1==dice2){
-    txt.textContent="No one wins";
+else{if (randompic1<randompic2){
+  //heading.textContent="Player 2 wins!";
+  console.log("player 2 wins")
 }
 else{
-    txt.textContent="Player 2 wins";
+ //heading.textContent="No one wins";
+ console.log("No one wins");
+}}
+button1.textContent="Try Again!";
 }
+
